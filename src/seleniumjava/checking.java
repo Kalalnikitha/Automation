5d1975810016a4e4c98c1f20724ce495ff14c59a
Nikitha <nikitha@example.com>
2025-06-01 17:45:21 +0530

    Newchanges

--- src/seleniumjava/checking.java
+++ src/seleniumjava/checking.java
@@ -6,10 +6,11 @@ public class checking {
    System.out.println("Radhika");
    System.out.println("ramesh");
     System.out.println("Ramya");
-
-
+    System.out.println("a");
+    System.out.println("rithika");
    System.out.println("x");
-    System.out.println("y");		
+    System.out.println("y");
+    System.out.println("R");
 
 	}
 
--- src/seleniumjava/printing.java
+++ src/seleniumjava/printing.java
@@ -5,10 +5,11 @@ public class printing {
 	public static void main(String[] args) {
 		System.out.println("My name is Nikitha");
 		System.out.println("NEW NAME");
-
+		System.out.println("NAME");
 		System.out.println("conflict");
-
+  //Nikitha123
 //this is for developers
+		
 
 	}
 
