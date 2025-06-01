d1ee5adf3ec5f9b5fe80d52a945e5839ffe17393
Kalalnikitha <nikithakalal003@gmail.com>
2025-06-01 17:28:33 +0530

    Update checking.java

--- src/seleniumjava/checking.java
+++ src/seleniumjava/checking.java
@@ -5,7 +5,8 @@ public class checking {
 	public static void main(String[] args) {
    System.out.println("Radhika");
    System.out.println("ramesh");
-    System.out.println("Ramya");		
+    System.out.println("Ramya");
+    System.out.println("Rajesh");		
 	}
 
 }
