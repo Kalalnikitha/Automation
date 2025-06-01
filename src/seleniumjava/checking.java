2fabc2d823a473c20bd7028e10c58d90f2df0d1c
Nikitha <nikitha@example.com>
2025-06-01 17:17:54 +0530

    referencecase

---
+++ src/seleniumjava/checking.java
@@ -0,0 +1,10 @@
+package seleniumjava;
+
+public class checking {
+
+	public static void main(String[] args) {
+   System.out.println("Radhika");
+   System.out.println("ramesh");
+	}
+
+}
--- src/seleniumjava/printing.java
+++ src/seleniumjava/printing.java
@@ -4,6 +4,7 @@ public class printing {
 
 	public static void main(String[] args) {
 System.out.println("My name is Nikitha");
+System.out.println("NEW NAME");
 
 //this is for developers
 
