fa1e32236d8b1472439f3e78c5f6db5340c100f0
Nikitha <nikitha@example.com>
2025-06-01 17:40:04 +0530

    Newchanges

--- src/seleniumjava/checking.java
+++ src/seleniumjava/checking.java
@@ -6,7 +6,9 @@ public class checking {
    System.out.println("Radhika");
    System.out.println("ramesh");
     System.out.println("Ramya");
-    System.out.println("Rajesh");		
+    System.out.println("Rajesh");	
+    System.out.println("Ramesh");	
+
 	}
 
 }
